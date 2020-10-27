c60083ad838be3de3ed615291d5f5661c81b06a5
vishal <vishalchavdaaugst@gmail.com>
2020-10-27 15:34:52 +0530

    removed appointment

--- appointmentt/src/main/java/com/online/appointmentt/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.online.appointmentt.repository;
-
-import com.online.appointmentt.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
