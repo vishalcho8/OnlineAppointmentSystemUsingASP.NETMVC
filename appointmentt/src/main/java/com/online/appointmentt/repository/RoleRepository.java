2eb3e961d55f838052c228573c07a8f410da9a74
vishal <vishalchavdaaugst@gmail.com>
2020-11-05 14:38:57 +0530

    Deleted java project from asp.net

--- appointmentt/src/main/java/com/online/appointmentt/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.online.appointmentt.repository;
-
-import com.online.appointmentt.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
--- appointmentt/src/main/java/com/online/appointmentt/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.online.appointmentt.service;
-
-import com.online.appointmentt.model.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
